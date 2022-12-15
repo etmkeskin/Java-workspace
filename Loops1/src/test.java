131310382433f4732df94b38a4fca0fbbef267fb
Etem Keskin <etmkeskin@gmail.com>
2022-12-15 12:09:18 -0500

    test1

--- Loops1/src/test.java
+++ Loops1/src/test.java
@@ -27,23 +27,26 @@ public class test {
 		Student p1 = new Student("Sam");
 		Student p2 = new Student("Tam");
 		//Student[] Students = new Student[2];
+		//System.out.println(Students[Students.length - 1] != null);
 		Student[] Students = {p1, p2};
+		//p1 = p2;
 		p1 = Students[1];
 		Students[0] = p2;
+		//System.out.println(Students[0] == p1);
 		p2.setName("Jam");
 		System.out.println(p1.getName());
-		System.out.println(Students[Students.length - 1].getName().equals("Tam"));
+		//System.out.println(Students[Students.length - 1].getName().equals("Tam"));
 		
-		boolean[][] x = new boolean[3][];
-		x[0] = new boolean[1]; 
-		x[1] = new boolean[2];
-		x[2] = new boolean[3];
-		System.out.println("x[2][2] is " + x[2][2]);
-		ArrayList<String> list = new ArrayList<String>();
-		int len = list.size();
-		list.add("test");
-		int len1 = list.size();
-		System.out.println(list.get(0) + "\t" + len + "\t" + len1);
+//		boolean[][] x = new boolean[3][];
+//		x[0] = new boolean[1]; 
+//		x[1] = new boolean[2];
+//		x[2] = new boolean[3];
+//		System.out.println("x[2][2] is " + x[2][2]);
+//		ArrayList<String> list = new ArrayList<String>();
+//		int len = list.size();
+//		list.add("test");
+//		int len1 = list.size();
+//		System.out.println(list.get(0) + "\t" + len + "\t" + len1);
 
 	}
 
