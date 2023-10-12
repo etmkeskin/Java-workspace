f11ba3549551c740bb05e316cca510751ce2aa51
Etem Keskin <etmkeskin@gmail.com>
2023-10-12 01:01:50 -0400

    Update

--- MatrixSum/src/MatrixElementsSum.java
+++ MatrixSum/src/MatrixElementsSum.java
@@ -1,18 +1,16 @@
 
 public class MatrixElementsSum {
 	
-	public int solution(int[][] matrix) {
+	public static int solution(int[][] matrix) {
 	    int sum = 0;
 	    int row = matrix.length;
 	    int col = matrix[0].length;
 	    
-	    int[] colZero = new int[row];
+	    //int[] colZero = new int[row];
 	    
 	    for(int i = 0; i < row; i++){
 	        for(int j = 0; j < col; j++){
-	            if(matrix[i][j] == 0){
-	                colZero[i] = j;
-	            }
+	        	sum += matrix[i][j];
 	        }
 	    }
 	    
@@ -22,7 +20,13 @@ public class MatrixElementsSum {
 	
 	
 	public static void main(String[] args) {
+		int[][] matrix = {{0, 1, 1, 2},
+						  {0, 5, 0, 0},
+						  {2, 0, 3, 3}};
 		
+		int sum = solution(matrix);
+
+		System.out.println(sum);
 		
 	}
 
