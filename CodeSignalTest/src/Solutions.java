4b9e0ddc76f64c01df7a415a1416759a7a0560ac
Etem Keskin <etmkeskin@gmail.com>
2023-10-12 16:42:19 -0400

    new class

---
+++ CodeSignalTest/src/Solutions.java
@@ -0,0 +1,31 @@
+
+public class Solutions {
+	
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
